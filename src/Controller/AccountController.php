84e5847dfe93542d8364be84417a23d0f41d92a3
dayaaan <dayaaan.vu@gmail.com>
2019-02-09 20:51:03 +0100

    gestion des utilisateurs et liaisons avec les annnonces

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\AccountType;
+use App\Entity\PasswordUpdate;
+use App\Form\RegistrationType;
+use App\Form\PasswordUpdateType;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AccountController extends AbstractController
+{
+    /**
+     * @Route("/login", name="account_login")
+     */
+    public function login(AuthenticationUtils $utils)
+    {
+        //gérer les erreurs
+        $error = $utils->getLastAuthenticationError();
+        //dump($error);
+        $username = $utils->getLastUsername();
+        return $this->render('account/login.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $username
+        ]);
+    }
+    /**
+     * @Route("/logout", name="account_logout")
+     */
+    public function logout() {
+       //rien
+    }
+    /**
+     * @Route("/register", name="account_register")
+     */
+    public function register(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder) {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //crypter le mot de passe
+            $hash = $encoder->encodePassword($user, $user->getHash());
+            $user->setHash($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "votre compte a bien été créé ! Vous pouvez maintenant vous connecter"
+            );
+            return $this->redirectToRoute('account_login');
+        }
+        return $this->render('account/registration.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+    /**
+     * Permet d'afficher et de traiter le formulaire de modification de profile
+     * @Route("/account/profile", name="account_profile")
+     * @return Response
+     */
+    public function profile(Request $request,ObjectManager $manager) {
+        $user = $this->getUser();
+        $form = $this->createForm(AccountType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Les données du profile ont bien été modifié !"
+            );
+        }
+
+        return $this->render('account/profile.html.twig',
+            [
+                'form' => $form->createView()
+            ]
+        );
+    }
+    /**
+     * Permet de modifier le mot de passe
+     * @Route("/account/password-update" ,name="account_password")
+     * @return Response
+     */
+    public function updatePassword(Request $request, UserPasswordEncoderInterface $encoder, ObjectManager $manager) {
+
+        $passwordUpdate = new PasswordUpdate();
+        $user = $this->getUser();
+        $form = $this->createForm(PasswordUpdateType::class, $passwordUpdate);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //vérifier que le old password du formulaire soit le meme password de l'user
+            if (!password_verify($passwordUpdate->getOldPassword(), $user->getHash())) {
+                //Gerer l'erreur, (personnalisez l'erreur)
+                $form->get('oldPassword')->addError(new FormError("Le mot de passe que vous avez taper n'est pas votre mot de passe actuel"));
+            } else {
+                $newPassword = $passwordUpdate->getNewPassword();
+                $hash = $encoder->encodePassword($user, $newPassword);
+                $user->setHash($hash);
+
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash(
+                    'success',
+                    "Votre mot de passe à bien été modifié"
+                );
+
+                return $this->redirectToRoute('homepage');
+            }
+         
+        }
+        return $this->render('account/password.html.twig',[
+            'form' => $form->createView()
+        ]);
+    } 
+    /**
+     * Permet d'afficher le profil de l'utilisateur connecté
+     * @Route("/account", name="account_index")
+     * @return Response
+     */
+
+    public function myAccount() {
+        return $this->render('user/index.html.twig', [
+            'user' => $this->getUser()
+        ]);
+    }  
+}
---
+++ src/Form/ApplicationType.php
@@ -0,0 +1,26 @@
+<?php 
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+
+//ApplicationType permet d'alléger le code de nos formulaires registrationType et annonceType
+class ApplicationType extends AbstractType {
+
+    /**
+     * Permet d'avoir la configuration de base d'un champ
+     *
+     * @param string $label
+     * @param string $placeholder
+     * @param array $options
+     * @return array
+     */
+    protected function getConfiguration($label,$placeholder, $options = [] ) {
+        return array_merge([
+            'label' => $label,
+                'attr' => [
+                    'placeholder' => $placeholder
+                ]
+        ], $options);
+    }
+}
